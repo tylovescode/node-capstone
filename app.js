881687ef30278e9804b701582789b92d114e063f
Ty Miller <ty.loves.code@gmail.com>
2018-05-05 23:35:12 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var MOCK_COLLECTION = {
+	"collectionItems": [
+	{
+		"id": "11111111",
+		"name": "Ten",
+		"artist": "Pearl Jam",
+		"format": "vinyl",
+		"notes": "lorem ipsum dolar",
+		"publishedAt": 1470016976609
+	},
+	{
+		"id": "22222222",
+		"name": "Superfuzz Bigmuff",
+		"artist": "Mudhoney",
+		"format": "vinyl",
+		"notes": "lorem ipsum dolar",
+		"publishedAt": 1470016976609
+	},
+	{
+		"id": "33333333",
+		"name": "Pulse",
+		"artist": "Pink Floyd",
+		"format": "vinyl",
+		"notes": "lorem ipsum dolar",
+		"publishedAt": 1470016976609
+	},
+	{
+		"id": "44444444",
+		"name": "Californication",
+		"artist": "Red Hot Chili Peppers",
+		"format": "vinyl",
+		"notes": "lorem ipsum dolar",
+		"publishedAt": 1470016976609
+	}]
+};
