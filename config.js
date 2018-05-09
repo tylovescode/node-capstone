e4fc8ce064f6e78517851d4f9dba9a5bf7faf7eb
Ty Miller <ty.loves.code@gmail.com>
2018-05-09 19:01:19 -0400

    Added configuration for port, datatbase url, and test database url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || //input local database here
-exports.TEST_DATABASE_URL - prcess.env.TEST_DATABASE_URL || //input local database here
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/'; //input local database here
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/'; //input local database here
 exports.PORT = process.env.PORT || 8080;
