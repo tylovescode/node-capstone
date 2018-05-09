48dd09055ab7a97c2b7368b84dafc0d5ec65375c
Ty Miller <ty.loves.code@gmail.com>
2018-05-09 19:53:35 -0400

    added route to local database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/'; //input local database here
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/'; //input local database here
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/nodeCapstoneDb';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-nodeCapstoneDb';
 exports.PORT = process.env.PORT || 8080;
