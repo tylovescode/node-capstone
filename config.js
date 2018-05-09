2801cfccd8cea649b018cf6b75906700df3d7b99
Ty Miller <ty.loves.code@gmail.com>
2018-05-09 18:47:35 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.DATABASE_URL = process.env.DATABASE_URL || //input local database here
+exports.TEST_DATABASE_URL - prcess.env.TEST_DATABASE_URL || //input local database here
+exports.PORT = process.env.PORT || 8080;
