d995892c44b84e0962f9fe98f2eac6139f93ad0c
Ty Miller <ty.loves.code@gmail.com>
2018-05-09 19:29:07 -0400

    Added initial code to require dependencies

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+const {Record} = require('./models');
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
