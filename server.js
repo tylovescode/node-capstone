43b495e90babeb9e591cf9f08950a0730a95cff0
Ty Miller <ty.loves.code@gmail.com>
2018-05-11 17:27:35 -0400

    Removed routes, added code for recordsRouter

--- server.js
+++ server.js
@@ -10,91 +10,99 @@ const Record = require('./models');
 
 const app = express();
 
+const recordsRouter = require('./recordsRouter');
+
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-//GET REQUEST TO API/RECORDS
-app.get('/api/records', (req, res) => {
-    Record
-    .find()
-    .sort({title: 1})
-    .then(records => {
-        res.json(records);
-    })
-    .catch(err => {
-    console.error(err);
-    res.status(500).json({message: 'Internal Server Error'});
-    });
+app.use('/api/records', recordsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'public/index.html');
 });
 
-//GET A RECORD BY ID
-app.get('/api/records/:id', (req, res) => {
-    Record
-    .findById(req.params.id)
-    .then(record => res.json(record))
-    .catch(err => {
-        console.error(err);
-        res.status(500).json({message: 'Internal Server Error'});
-    });
-});
+// //GET REQUEST TO API/RECORDS
+// app.get('/api/records', (req, res) => {
+//     Record
+//     .find()
+//     .sort({title: 1})
+//     .then(records => {
+//         res.json(records);
+//     })
+//     .catch(err => {
+//     console.error(err);
+//     res.status(500).json({message: 'Internal Server Error'});
+//     });
+// });
 
-//ADD A RECORD
-app.post('/api/records', (req, res) => {
-    const requiredFields = ['title', 'artist', 'format'];
-    for (let i = 0; i < requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing ${field} in request body`;
-            console.error(message);
-            return res.status(400).send(message);
-        }
-    }
-    Record
-        .create({
-            title: req.body.title,
-            artist: req.body.artist,
-            format: req.body.format,
-            genre: req.body.genre,
-            release_date: req.body.release_date,
-            image_url: req.body.image_url
-        })
-        .then(record => res.status(201).json(record))
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({message: 'Internal Server Error'});
-        });
-});
+// //GET A RECORD BY ID
+// app.get('/api/records/:id', (req, res) => {
+//     Record
+//     .findById(req.params.id)
+//     .then(record => res.json(record))
+//     .catch(err => {
+//         console.error(err);
+//         res.status(500).json({message: 'Internal Server Error'});
+//     });
+// });
 
-//UPDATE A RECORD
-app.put('/api/records/:id', (req, res) => {
-  //Ensure the ids in the request path and request body match
-  if (!(req.params.id && req.body.id === req.body.id)) {
-    const message = (`Request path id ${req.params.id} and request body id ${req.body.id} must match`);
-    console.error(message);
-    return res.status(400).json({ message: message });
-  }
-  const toUpdate = {};
-  const updateableFields = ['title', 'artist', 'format', 'genre', 'release_date', 'image_url'];
+// //ADD A RECORD
+// app.post('/api/records', (req, res) => {
+//     const requiredFields = ['title', 'artist', 'format'];
+//     for (let i = 0; i < requiredFields.length; i++) {
+//         const field = requiredFields[i];
+//         if (!(field in req.body)) {
+//             const message = `Missing ${field} in request body`;
+//             console.error(message);
+//             return res.status(400).send(message);
+//         }
+//     }
+//     Record
+//         .create({
+//             title: req.body.title,
+//             artist: req.body.artist,
+//             format: req.body.format,
+//             genre: req.body.genre,
+//             release_date: req.body.release_date,
+//             image_url: req.body.image_url
+//         })
+//         .then(record => res.status(201).json(record))
+//         .catch(err => {
+//             console.error(err);
+//             res.status(500).json({message: 'Internal Server Error'});
+//         });
+// });
 
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      toUpdate[field] = req.body[field];
-    }
-  });
-  Record
-  //All key value pairs in toUpdate will be updated - that's what $set does
-  .findByIdAndUpdate(req.params.id, { $set: toUpdate })
-  .then(record => res.status(204).end())
-  .catch(err => res.status(500).json({message: 'Internal Server Error'}));
-});
+// //UPDATE A RECORD
+// app.put('/api/records/:id', (req, res) => {
+//   //Ensure the ids in the request path and request body match
+//   if (!(req.params.id && req.body.id === req.body.id)) {
+//     const message = (`Request path id ${req.params.id} and request body id ${req.body.id} must match`);
+//     console.error(message);
+//     return res.status(400).json({ message: message });
+//   }
+//   const toUpdate = {};
+//   const updateableFields = ['title', 'artist', 'format', 'genre', 'release_date', 'image_url'];
 
-//DELETE A RECORD
-app.delete('/api/records/:id', (req, res) => {
-  Record
-  .findByIdAndRemove(req.params.id)
-  .then(record => res.status(204).end())
-  .catch(err => res.status(500).json({message: 'Internal Server Error'}));
-});
+//   updateableFields.forEach(field => {
+//     if (field in req.body) {
+//       toUpdate[field] = req.body[field];
+//     }
+//   });
+//   Record
+//   //All key value pairs in toUpdate will be updated - that's what $set does
+//   .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+//   .then(record => res.status(204).end())
+//   .catch(err => res.status(500).json({message: 'Internal Server Error'}));
+// });
+
+// //DELETE A RECORD
+// app.delete('/api/records/:id', (req, res) => {
+//   Record
+//   .findByIdAndRemove(req.params.id)
+//   .then(record => res.status(204).end())
+//   .catch(err => res.status(500).json({message: 'Internal Server Error'}));
+// });
 
 // closeServer needs access to a server object, but that only
 // gets created when `runServer` runs, so we declare `server` here
