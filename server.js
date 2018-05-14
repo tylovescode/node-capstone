25a6156e59af0a57f4b40adff4bc4d70e2f5eca8
Ty Miller <ty.loves.code@gmail.com>
2018-05-14 19:33:47 -0400

    Added urlencoded to body parser - form was not passing data

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ passport.use(jwtStrategy);
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/records/', recordsRouter);
 app.use('/api/users/', usersRouter);
